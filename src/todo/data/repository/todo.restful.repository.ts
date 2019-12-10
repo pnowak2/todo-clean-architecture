b8841c8622a2bab512107a703194ac97558f80d7
Piotr Nowak <p.nowak2@gmail.com>
2019-12-10 23:11:38 +0100

    fixed axios

--- src/todo/data/repository/todo.restful.repository.ts
+++ src/todo/data/repository/todo.restful.repository.ts
@@ -6,9 +6,9 @@ import  * as axios from "axios";
 
 export class TodoRestfulRepository implements TodoRepository {
   getAllTodos(): Observable<Array<Todo>> {
-    return from(axios.default.get('http://www.mocky.io/v2/5df002e02f000038c48e0edc')).pipe(
+    return from(axios.default.get('http://www.mocky.io/v2/5df017ae2f000038c48e0f1d')).pipe(
       map(res => res.data.body.todos),
-      map(todos => todos.map((todojson: any) => { name: 'test'}))
+      map(todos => todos.map((item: any) => ({ name: item })))
     );
   }
 }
