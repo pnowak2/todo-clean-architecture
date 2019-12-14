1b847924594a42051d1af5a68147a02d0f709e87
Piotr Nowak <p.nowak2@gmail.com>
2019-12-14 21:19:08 +0100

    both terminal and vanillajs running

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { VanillaJsApp } from "./apps/vanillajs/main.app";
 import { TerminalApp } from "./apps/terminal/main.app";
 
 new TerminalApp().run();
-// new VanillaJsApp().run();
+new VanillaJsApp().run();
