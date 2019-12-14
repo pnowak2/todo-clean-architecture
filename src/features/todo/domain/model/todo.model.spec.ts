a65c2cdc125b0c98fa2481d6d5e6cac903e15ff8
Piotr Nowak <p.nowak2@gmail.com>
2019-12-14 22:02:21 +0100

    running tests working

---
+++ src/features/todo/domain/model/todo.model.spec.ts
@@ -0,0 +1,13 @@
+import { Todo } from "./todo.model";
+
+test('Todo Model', () => {
+  const model = new Todo({
+    id: '1',
+    name: 'foo',
+    completed: true
+  });
+
+  expect(model.id).toEqual('1');
+  expect(model.name).toEqual('foo');
+  expect(model.completed).toEqual(true);
+});
