55b5b062ada68b8ab5b23bb87a8818002d1dbfa2
Piotr Nowak <p.nowak2@gmail.com>
2019-12-17 14:29:53 +0100

    mocking idea for presenter test

--- src/features/todo/presentation/todo-default.presenter.spec.ts
+++ src/features/todo/presentation/todo-default.presenter.spec.ts
@@ -1,34 +1,40 @@
+import { of } from 'rxjs';
+import { TodoRepository } from '../domain/repository/todo.repository';
 import { GetAllTodosUseCase } from '../domain/usecase/get-all-todos.usecase';
 import { TodoDefaultPresenter } from './todo-default.presenter';
 import { TodoPresenter } from './todo.presenter';
+import { skip } from 'rxjs/operators';
 
 describe('Todo Presenter', () => {
   let todoPresenter: TodoPresenter;
 
   beforeEach(() => {
-    const repo = null;
-    const getAllUC = new GetAllTodosUseCase(repo);
-    todoPresenter = new TodoDefaultPresenter(
-      getAllUC,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null
-    );
+    const getAllUC = new GetAllTodosUseCase(null);
+    jest.spyOn(getAllUC, 'execute').mockImplementation(() => {
+      return of([
+        { id: '1', name: 'foo' },
+        { id: '2', name: 'bar' },
+      ]);
+    })
+
+    todoPresenter = new TodoDefaultPresenter(getAllUC, null, null, null, null, null, null, null, null, null);
   });
 
-  describe('.getAllTodos()', () => {
-    it('should return all items', (done) => {
+  describe('🍕, 🍅, 🧀, 🌶️, 🍄', () => {
+    it('should initially return empty array', (done) => {
       todoPresenter.todos$.subscribe(todos => {
-        expect(true).toBe(true);
+        expect(todos).toHaveLength(0);
         done();
       });
-      // todoPresenter.getAllTodos();
+    });
+
+    it('should return proper items on getting all items', (done) => {
+      todoPresenter.todos$.pipe(skip(1)).subscribe(todos => {
+        expect(todos).toHaveLength(2);
+        done();
+      });
+
+      todoPresenter.getAllTodos();
     });
   });
 });
