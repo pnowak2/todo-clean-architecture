db127c280acabb20c5ac2af479165a3f693f84a8
Piotr Nowak <p.nowak2@gmail.com>
2019-12-17 19:18:54 +0100

    search todos usecase

--- src/features/todo/domain/usecase/search-todos.usecase.ts
+++ src/features/todo/domain/usecase/search-todos.usecase.ts
@@ -3,10 +3,18 @@ import { UseCase } from '../../../../core/domain/usecase/usecase';
 import { TodoEntity } from '../entity/todo.entity';
 import { TodoRepository } from '../repository/todo.repository';
 
-export class SearchTodosUseCase implements UseCase<string, TodoEntity[]> {
-  constructor(private todoRepository: TodoRepository) {}
+export type FilterType = 'active' | 'completed' | null;
 
-  execute(keyword: string): Observable<TodoEntity[]> {
-    return this.todoRepository.searchTodos(keyword);
+export class SearchTodosUseCase implements UseCase<FilterType, TodoEntity[]> {
+  constructor(private todoRepository: TodoRepository) { }
+
+  execute(filterType: FilterType): Observable<TodoEntity[]> {
+    if (filterType === 'active') {
+      return this.todoRepository.getIncompletedTodos();
+    } else if (filterType === 'completed') {
+      return this.todoRepository.getCompletedTodos();
+    } else {
+      return this.todoRepository.getAllTodos();
+    }
   }
 }
