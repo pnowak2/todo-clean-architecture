453df3a116d64c1d355237e51b4862715badaa91
Piotr Nowak <p.nowak2@gmail.com>
2019-12-19 22:54:53 +0100

    feat(core): reexporting public api

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from './src/features/todo/data/repository/inmemory/todo.inmemory.repository';
+export * from './src/features/todo/data/repository/localstorage/todo.localstorage.repository';
+export * from './src/features/todo/presentation/viewmodel/todos.viewmodel';
+export * from './src/features/todo/domain/repository/todo.repository';
+export * from './src/features/todo/presentation/presenter/todo.presenter';
+export * from './src/features/todo/presentation/presenter/todo-default.presenter';
