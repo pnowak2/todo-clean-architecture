381be81e62f48279f7f469d3447299471d300e96
Piotr Nowak <p.nowak2@gmail.com>
2019-12-19 22:59:16 +0100

    chore(core): exports and version bump

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './features/todo/data/repository/inmemory/todo.inmemory.repository';
-export * from './features/todo/presentation/viewmodel/todos.viewmodel';
+export * from './features/todo/data/repository/localstorage/todo.localstorage.repository';
 export * from './features/todo/domain/repository/todo.repository';
+export * from './features/todo/presentation/presenter/todo-default.presenter';
 export * from './features/todo/presentation/presenter/todo.presenter';
+export * from './features/todo/presentation/viewmodel/todos.viewmodel';
