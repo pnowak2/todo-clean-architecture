641a37dfb8bedc3c722ec0dc7474e66bde2a283a
Piotr Nowak <p.nowak2@gmail.com>
2020-01-08 14:11:04 +0100

    fix(ddd): unique id generation fix

--- src/shared/domain/unique-entity-id.ts
+++ src/shared/domain/unique-entity-id.ts
@@ -2,6 +2,6 @@ import { Identifier } from './identifier'
 
 export class UniqueEntityID extends Identifier<string | number>{
     constructor(id?: string | number) {
-        super(id ? id : Math.random.toString())
+        super(id ? id : Math.random().toString())
     }
 }
