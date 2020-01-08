9ed4b976ab7240f37c0e288705d9485ec6f5c59d
Piotr Nowak <p.nowak2@gmail.com>
2020-01-08 08:17:48 +0100

    refactor(map): mapper using entity factory method

--- src/features/todo/data/repository/inmemory/mapper/todo-mock.mapper.ts
+++ src/features/todo/data/repository/inmemory/mapper/todo-mock.mapper.ts
@@ -4,10 +4,20 @@ import { TodoMockDto } from '../dto/todo-mock.dto';
 
 export class TodoMockMapper implements Mapper<TodoEntity, TodoMockDto> {
   mapFrom(input: TodoEntity): TodoMockDto {
-    return { id: input.id, title: input.name, completed: input.completed };
+    return {
+      id: input.id,
+      title: input.name,
+      completed: input.completed
+    };
   }
 
   mapTo(input: TodoMockDto): TodoEntity {
-    return { id: input.id, name: input.title, completed: input.completed };
+    const todo = TodoEntity.create({
+      id: input.id,
+      name: input.title,
+      completed: input.completed
+    });
+
+    return todo;
   }
 }
